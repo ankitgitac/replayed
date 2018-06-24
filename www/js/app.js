0b35c9df6871dbe6aaeaab4b6e3b18e8995e9334
Ankit Gupta <ankit.gupta01@nagarro.com>
2018-06-24 22:29:37 +0530

    renamed module name to replay

--- www/js/app.js
+++ www/js/app.js
@@ -1,10 +1,10 @@
-// Ionic Starter App
+// Ionic Replay App
 
 // angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// 'replay' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-// 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers'])
+// 'replay.controllers' is found in controllers.js
+angular.module('replay', ['ionic', 'replay.controllers'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,4 +1,4 @@
-angular.module('starter.controllers', [])
+angular.module('replay.controllers', [])
 
 .controller('AppCtrl', function($scope, $ionicModal, $timeout) {
 
